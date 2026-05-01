9d96785e70b6ae62e13aa471805ba828a9ca4c1e
TofikCilik <33724406+tofikcilik@users.noreply.github.com>
2026-05-01 17:35:55 +0700

    feat: seed administrator and staff identity fields

--- backend/database/seeders/DemoUserSeeder.php
+++ backend/database/seeders/DemoUserSeeder.php
@@ -13,17 +13,28 @@ class DemoUserSeeder extends Seeder
 {
     public function run(): void
     {
-        $district = District::updateOrCreate(['code' => 'KEC-001'], ['name' => 'Kecamatan Sukamaju']);
+        $district = District::updateOrCreate(['code' => 'KEC001'], ['name' => 'Kecamatan Sukamaju']);
         $village = Village::updateOrCreate(
-            ['code' => 'DES-001'],
+            ['code' => 'DES001'],
             ['district_id' => $district->id, 'name' => 'Desa Sumber Jaya']
         );
 
         $users = [
             [
+                'name' => 'Root Administrator',
+                'email' => 'admin@airbersih.test',
+                'role' => 'administrator',
+                'phone' => '081200000001',
+                'service_area' => 'Seluruh kecamatan dan desa',
+                'district_id' => null,
+                'village_id' => null,
+            ],
+            [
                 'name' => 'Operator Kecamatan',
                 'email' => 'kecamatan@airbersih.test',
                 'role' => 'kecamatan',
+                'phone' => '081200000002',
+                'service_area' => 'Kecamatan Sukamaju',
                 'district_id' => $district->id,
                 'village_id' => null,
             ],
@@ -31,6 +42,8 @@ class DemoUserSeeder extends Seeder
                 'name' => 'Operator Desa',
                 'email' => 'desa@airbersih.test',
                 'role' => 'desa',
+                'phone' => '081200000003',
+                'service_area' => 'Desa Sumber Jaya',
                 'district_id' => $district->id,
                 'village_id' => $village->id,
             ],
@@ -38,6 +51,8 @@ class DemoUserSeeder extends Seeder
                 'name' => 'Petugas Lapangan',
                 'email' => 'petugas@airbersih.test',
                 'role' => 'petugas_lapangan',
+                'phone' => '081200000004',
+                'service_area' => 'RT 001 sampai RT 004 Desa Sumber Jaya',
                 'district_id' => $district->id,
                 'village_id' => $village->id,
             ],
@@ -49,6 +64,8 @@ class DemoUserSeeder extends Seeder
                 [
                     'name' => $user['name'],
                     'password' => Hash::make('password'),
+                    'phone' => $user['phone'],
+                    'service_area' => $user['service_area'],
                     'role_id' => Role::where('name', $user['role'])->value('id'),
                     'district_id' => $user['district_id'],
                     'village_id' => $user['village_id'],
