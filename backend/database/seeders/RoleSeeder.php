bddcbdf292d85f71e60bf3fc77d171a802f47880
TofikCilik <33724406+tofikcilik@users.noreply.github.com>
2026-05-01 17:33:59 +0700

    feat: add administrator role seed

--- backend/database/seeders/RoleSeeder.php
+++ backend/database/seeders/RoleSeeder.php
@@ -10,6 +10,7 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         collect([
+            ['name' => 'administrator', 'label' => 'Root Administrator'],
             ['name' => 'kecamatan', 'label' => 'Operator Kecamatan'],
             ['name' => 'desa', 'label' => 'Operator Desa'],
             ['name' => 'petugas_lapangan', 'label' => 'Petugas Lapangan'],
